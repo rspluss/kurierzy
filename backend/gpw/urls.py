4b9ba148f00b1b5787b7ab57f7ba940fed8f2689
Dawid Machala <iduopro@gmail.com>
2022-11-14 08:46:10 +0100

    user authenticate, user registration, logout

--- backend/gpw/urls.py
+++ backend/gpw/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('download_index/', views.download_index, name="download_index"),
     path('api/index-list/', views.IndexView.as_view(), name="index_view"),
     path('table/', views.table, name="table"),
+    path('login_user/', views.login_user, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('register_user/', views.register_user, name="register_user"),
 ]
--- backend/gpw/views.py
+++ backend/gpw/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Index
 from .tasks import download
 from .serializers import IndexSerializer
@@ -19,6 +22,14 @@ class IndexView(generics.RetrieveAPIView):
 def home(request):
     indexes = Index.objects.all()
 
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
     context = {'indexes': indexes}
     return render(request, "gpw/index.html", context)
 
@@ -35,3 +46,39 @@ def table(request):
 
     indexes = Index.objects.all()
     return render(request, 'gpw/table.html', {'indexes': indexes})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+    context = {}
+    return render(request, "authenticate/login.html", context)
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, "authenticate/register_user.html", context)
+
+
+def logout_view(request):
+    logout(request)
+
+    return redirect('home')
